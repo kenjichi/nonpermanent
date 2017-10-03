d8388afcb12a29e4536b2c4d0e04e62f81639d6f
kenjichi <32210147+kenjichi@users.noreply.github.com>
2017-10-02 18:57:47 -0700

    Update register.js

--- imports/plugins/custom/mytheme/register.js
+++ imports/plugins/custom/mytheme/register.js
@@ -6,4 +6,22 @@ Reaction.registerPackage({
   name: "mytheme",
   icon: "fa fa-vine",
   autoEnable: true
+  layout: [{
+    layout: "coreLayoutNonpermanent",
+    workflow: "coreWorkflow",
+    collection: "Products",
+    theme: "default",
+    enabled: true,
+    structure: {
+      template: "productsLanding",
+      layoutHeader: "layoutHeader",
+      layoutFooter: "layoutFooter",
+      notFound: "productNotFound",
+      dashboardHeader: "",
+      dashboardControls: "dashboardControls",
+      dashboardHeaderControls: "",
+      adminControlsFooter: "adminControlsFooter"
+    }
+  }]
+  
 });
